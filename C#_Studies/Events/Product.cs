fcc39a0e9ac1f2e3c006fe5d156bbd458bd2a6a1
Zeynep Aydinli <aydinli.zeynep@gmail.com>
2024-06-24 14:14:02 +0300

    Events updated

---
+++ C#_Studies/Events/Product.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events;
+
+public delegate void StockControl(); //delegate
+
+public class Product
+{
+    private int _stock;
+
+    public Product(int stock)
+    {
+        _stock = stock;
+    }
+
+    public event StockControl StockControlEvent; //event
+
+    public string ProductName { get; set; }
+    public int Stock {
+        get
+        {
+            return _stock;
+        }
+        set
+        {
+            _stock = value;
+            if (value <= 15 && StockControlEvent != null)
+            {
+                StockControlEvent();
+            }
+        }
+     }       
+
+    public void Sell(int amount)
+    {
+        Stock = Stock - amount; 
+        Console.WriteLine("Stock amount: " + Stock + " " + ProductName);
+    }
+}
---
+++ C#_Studies/Events/Program.cs
@@ -0,0 +1,22 @@
+﻿using Events;
+
+Product harddisk = new Product(50);
+harddisk.ProductName = "Hard Disk";
+
+Product gsm = new Product(50);
+gsm.ProductName = "GSM";
+gsm.StockControlEvent += Gsm_StockControlEvent;
+
+void Gsm_StockControlEvent() //Otomatik gelir
+{
+    Console.WriteLine("Gsm about to finish");
+}
+
+for (int i = 0; i < 10; i++)
+{
+    harddisk.Sell(10);
+    gsm.Sell(10);
+    Console.ReadLine();
+}
+
+Console.ReadLine();
