739e98da496f361fd7e895cd8c9823e94d1281c9
Zeynep Aydinli <aydinli.zeynep@gmail.com>
2024-06-24 12:53:04 +0300

    Reflection updated

---
+++ C#_Studies/Reflection/Maths.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection;
+
+public class Maths
+{
+    private int _number1;
+    private int _number2;
+
+    public Maths(int number1, int number2)
+    {
+        _number1 = number1;
+        _number2 = number2;
+    }
+    public int Add(int number1,  int number2)
+    {
+        return number1 + number2;
+    }
+
+    public int Multiplication(int number1, int number2)
+    {
+        return number1 * number2;
+    }
+
+    public int Add2()
+    {
+        return _number1 + _number2;
+    }
+
+    public int Multiplication2()
+    {
+        return _number1 * _number2;
+    }
+
+}
---
+++ C#_Studies/Reflection/Program.cs
@@ -0,0 +1,32 @@
+﻿using Reflection;
+using System.Reflection;
+
+//Maths maths = new Maths(2, 3);
+
+//Console.WriteLine(maths.Add2());
+//Console.WriteLine(maths.Add(3, 4));
+
+var type = typeof(Maths);
+
+//Maths maths = (Maths)Activator.CreateInstance(type, 6, 7); 
+
+//Console.WriteLine(maths.Add(4, 5));
+//Console.WriteLine(maths.Add2());
+
+var instance = Activator.CreateInstance(type, 6 ,5); //**Activator.CreateInstance obje döndürür
+
+MethodInfo methodInfo = instance.GetType().GetMethod("Add2");
+
+Console.WriteLine(methodInfo.Invoke(instance, null));
+
+Console.WriteLine("---------------------------------");
+
+var methods = type.GetMethods();
+
+foreach (var method in methods)
+{
+    Console.WriteLine("Method Name: " + method.Name);
+}
+
+Console.ReadLine();
+
